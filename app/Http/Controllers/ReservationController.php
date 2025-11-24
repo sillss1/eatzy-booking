810e420a2c825c8889abc6a082c8737a17cdcce2
Igor Orlinski <igor.orlinski@student.pk.edu.pl>
2025-11-24 19:53:00 +0000

    Bugfixes of restaurant and account deletions, added error messages

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -32,11 +32,15 @@ class ReservationController extends Controller
 
             $query = Reservation::query()
                 ->where('restaurant_id', $selectedRestaurant)
+                ->whereNotNull('user_id')
+                ->whereNotNull('restaurant_id')
                 ->with(['restaurant', 'user']);
 
         } else {
             $query = Reservation::query()
                 ->where('user_id', $user->id)
+                ->whereNotNull('user_id')
+                ->whereNotNull('restaurant_id')
                 ->with(['restaurant', 'user']);
         }
         
